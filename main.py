8efd425563ad80f77e196d48d5da02cc20ff2392
Jay W <coding@jaywilliams.me>
2016-01-17 20:15:34 +0000

    I removed it as I realised I don't need it

--- main.py
+++ main.py
@@ -58,9 +58,6 @@ def getHelp():
     print(" auth - Shows a login prompt for testing purposes.")
 class Oopsy(Exception):
     pass
-def argGetter(string):
-    splitted = string.split(" ")
-    return splitted[1:]
 class auth():
     def auth():
         if not os.path.isfile("logins.json"):
